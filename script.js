87f5162eca6781eef033d86120ba8204e37e840e
wofisdk <cody.constine@colorado.edu>
2016-05-22 10:45:11 -0600

    Changed the background and redid some colors

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
 	$('.button').hover(
 		function(){
-			$(this).css('background-color', '#000000');
+			$(this).css('background-color', '#00a3cc');
 			$(this).css('color', '#f7f7f7');			
 		},
 		function(){
-			$(this).css('background-color', '#2226B6');
+			$(this).css('background-color', '#00ccff');
 			$(this).css('color', '#000000');
+		}
+	);
+	$('.bubble').hover (
+		function(){
+			$(this).css('height', '67%');
+			$(this).css('width','52%');
+			$(this).css('background-color', '#00ccff');
+		},
+		function(){
+			$(this).css('height', '65%');
+			$(this).css('width','50%');
+			$(this).css('background-color', '#003380');
 		}
 	);
 });
