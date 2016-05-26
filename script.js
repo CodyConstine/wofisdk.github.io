0ef6d202a8300530b2e15c5ecb14f5e65344d17a
wofisdk <cody.constine@colorado.edu>
2016-05-26 11:02:21 -0600

    Finished bubbles

--- script.js
+++ script.js
@@ -12,10 +12,30 @@ $(document).ready(function(){
 	$('.bubble').hover ( //Hover function for bubble buttons
 		function(){
 			var id = $(this).attr('id');
+			if(id === 'about_bub') //decide which bubble is being hovered over
+			{
+				$('#about_bub div').css('background-color', '#00a3cc');
+				$('#about_bub div').css('color', '#f7f7f7');
+			}
+			else
+			{
+				$('#contact_bub div').css('background-color', '#00a3cc');
+				$('#contact_bub div').css('color', '#f7f7f7');				
+			}
 			$(this).css('background-color', '#00ccff');
-			$("."+id+" div").css('background-color', "#003380");
 		},
 		function(){
+			var id = $(this).attr('id');
+			if(id === 'about_bub') //decide which bubble is being hovered over
+			{
+				$('#about_bub div').css('background-color', '#00ccff');
+				$('#about_bub div').css('color', '#000000');
+			}
+			else
+			{
+				$('#contact_bub div').css('background-color', '#00ccff');
+				$('#contact_bub div').css('color', '#000000');				
+			}
 			$(this).css('background-color', '#003380');
 		}
 	);
