45a324d3ce0af04c822820b3c41db188d6d0f53d
cody constine <cody.constine@colorado.edu>
2016-08-24 20:36:49 -0600

    remove bubbles

--- script.js
+++ script.js
@@ -9,34 +9,4 @@ $(document).ready(function(){
 			$(this).css('color', '#000000');
 		}
 	);
-	$('.bubble').hover ( //Hover function for bubble buttons
-		function(){
-			var id = $(this).attr('id');
-			if(id === 'about_bub') //decide which bubble is being hovered over
-			{
-				$('#about_bub div').css('background-color', '#00a3cc');
-				$('#about_bub div').css('color', '#f7f7f7');
-			}
-			else
-			{
-				$('#contact_bub div').css('background-color', '#00a3cc');
-				$('#contact_bub div').css('color', '#f7f7f7');				
-			}
-			$(this).css('background-color', '#00ccff');
-		},
-		function(){
-			var id = $(this).attr('id');
-			if(id === 'about_bub') //decide which bubble is being hovered over
-			{
-				$('#about_bub div').css('background-color', '#00ccff');
-				$('#about_bub div').css('color', '#000000');
-			}
-			else
-			{
-				$('#contact_bub div').css('background-color', '#00ccff');
-				$('#contact_bub div').css('color', '#000000');				
-			}
-			$(this).css('background-color', '#003380');
-		}
-	);
 });
