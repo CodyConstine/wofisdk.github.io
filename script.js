cad089925466959e0ade77f61e2d19a66c401621
cody constine <cody.constine@colorado.edu>
2016-11-28 15:23:07 -0700

    Init commit

--- script.js
+++ script.js
@@ -1,12 +1,79 @@
-$(document).ready(function(){
-	$('.button').hover( //Hover function for header buttons
-		function(){
-			$(this).css('background-color', '#00a3cc');
-			$(this).css('color', '#f7f7f7');			
-		},
-		function(){
-			$(this).css('background-color', '#00ccff');
-			$(this).css('color', '#000000');
-		}
-	);
-});
+(function ($) {
+    var homePage = null;
+    var dayPage = null;
+    var nightPage = null;
+    var recruitPage = null;
+
+    var homeTab = null;
+    var dayTab = null;
+    var nightTab = null;
+    var recruitTab = null;
+
+
+    var goToHome = function(){
+        homePage.show();
+        dayPage.hide();
+        nightPage.hide();
+        recruitPage.hide();
+
+        homeTab.parent().addClass("active");
+        dayTab.parent().removeClass("active");
+        nightTab.parent().removeClass("active");
+        recruitTab.parent().removeClass("active");
+    };
+
+    var goToDay = function(){
+        homePage.hide();
+        dayPage.show();
+        nightPage.hide();
+        recruitPage.hide();
+
+        homeTab.parent().removeClass("active");
+        dayTab.parent().addClass("active");
+        nightTab.parent().removeClass("active");
+        recruitTab.parent().removeClass("active");
+    };
+
+    var goToNight = function(){
+        homePage.hide();
+        dayPage.hide();
+        nightPage.show();
+        recruitPage.hide();
+
+        homeTab.parent().removeClass("active");
+        dayTab.parent().removeClass("active");
+        nightTab.parent().addClass("active");
+        recruitTab.parent().removeClass("active");
+    };
+
+    var goToRecruit = function(){
+        homePage.hide();
+        dayPage.hide();
+        nightPage.hide();
+        recruitPage.show();
+
+        homeTab.parent().removeClass("active");
+        dayTab.parent().removeClass("active");
+        nightTab.parent().removeClass("active");
+        recruitTab.parent().addClass("active");
+    };
+
+    $( document ).ready(function() {
+        homePage = $("#homePage");
+        dayPage = $("#dayPage");
+        nightPage = $("#nightPage");
+        recruitPage = $("#recruitPage");
+
+        homeTab = $("#homeTab");
+        dayTab = $("#dayTab");
+        nightTab = $("#nightTab");
+        recruitTab = $("#recruitTab");
+
+        goToHome();
+
+        homeTab.on("click", goToHome);
+        dayTab.on("click", goToDay);
+        nightTab.on("click", goToNight);
+        recruitTab.on("click", goToRecruit);
+    });
+}(jQuery));
